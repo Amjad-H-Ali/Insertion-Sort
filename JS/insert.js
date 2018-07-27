76bff9d73c2d7cad15240155bde2994d6948e961
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-07-27 04:23:30 -0500

    Added logic so function iterates through array starting from index one

--- JS/insert.js
+++ JS/insert.js
@@ -1,1 +1,10 @@
 console.log('Insertion Sort!');
+
+const insertionSort = (arr) => {
+
+	for (let i = 1, len = arr.length; i < len; i ++) {
+		console.log(arr[i]);
+	}
+};
+
+insertionSort([0, 0, 2, 2, 2, 2, 3, 4, 5, 5, 5, 5, 6, 6, 7, 7, 9, 9, 34, 43, 45, 99]);
